a3ba9592a1c01908dee052951c22c11276356d2b
root <root@localhost.localdomain>
2018-10-21 22:49:47 +0300

    deployment

--- hood/urls.py
+++ hood/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include,
+from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth import views 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^' ,include('neighbourhood.urls',name='index')),
+    url(r'^$', name='index'),
+    url(r'^' ,include('neighbourhood.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls'),{"next_page":'/'}),
     url(r'^login/$', views.login, name='login'),
     url(r'^auth/', include('social_django.urls', namespace='social')),  # <- Here
