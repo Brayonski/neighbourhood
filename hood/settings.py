c506dc857a74f9039882a3c8265b501d98c1435a
root <root@localhost.localdomain>
2018-10-21 22:33:37 +0300

    login redirect

--- hood/settings.py
+++ hood/settings.py
@@ -184,7 +184,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = '/'
 
 #  Add configuration for static files storage using whitenoise
 
