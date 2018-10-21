249c06f38cfb2119a003614624acf113fd34e3ee
root <root@localhost.localdomain>
2018-10-21 20:47:26 +0300

    configure static parameters

--- hood/settings.py
+++ hood/settings.py
@@ -174,8 +174,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
