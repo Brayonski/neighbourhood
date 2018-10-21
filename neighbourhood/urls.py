b7456c94fc892cde303917a7e2367811847cd945
root <root@localhost.localdomain>
2018-10-21 20:01:21 +0300

    app urls

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -5,7 +5,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
- 
+    url(r'^new/post$', views.new_post, name='new-post'),
+    url(r'^profile/(?P<user_id>\d+)?$', views.profile, name='profile'),
+    url(r'^update/profile$', views.updateprofile, name='updateprofile'),
+    url(r'^addcommunity/(?P<user_id>\d+)?$', views.location, name='add-community'), 
+    url(r'^createbusiness/(?P<user_id>\d+)?$', views.create_business, name='create-business'),  
+    url(r'^search/', views.search_results, name='search_results'),
 ]
 
 if settings.DEBUG:
